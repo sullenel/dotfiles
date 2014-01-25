705b2a30668f7f0bbf4a90ecacf559a3017affeb
sullenel <sullenerd@gmail.com>
2014-01-25 17:57:46 +0400

    Change prompt colour

--- zsh/prompts.zsh
+++ zsh/prompts.zsh
@@ -5,5 +5,5 @@
 local count_files='$(ls -A | wc -l)'
 
 
-PROMPT="%{$fg_bold[cyan]%}%2~%{$fg_bold[red]%} ➜ %{$reset_color%}"
+PROMPT="%{$fg_bold[blue]%}%2~%{$fg_bold[red]%} ➜ %{$reset_color%}"
 RPROMPT="%{$fg[green]%}${count_files}%{$reset_color%}"
