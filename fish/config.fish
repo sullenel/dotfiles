812ef56834ee064de8f986388a7245ca51590ffd
sullenel <sullenerd@gmail.com>
2014-06-09 07:49:47 +0400

    Change prompt

--- fish/config.fish
+++ fish/config.fish
@@ -39,13 +39,19 @@ set -x TERM xterm-256color
 # -----------------------------------------------------------------------------
 # LPrompt
 function fish_prompt
+    function _last_two_dirs
+      if [ $PWD != $HOME ];
+        echo -n $PWD | awk -F\/ '{print $(NF-1),$(NF)}' | sed 's/ /\\//'
+      end
+    end
+
     set -l red (set_color -o red)
     set -l blue (set_color -o blue)
     set -l normal (set_color normal)
     set -l arrow "$red➜"
-    set -l cwd $blue(prompt_pwd)
+    set -l cwd $blue(_last_two_dirs)
 
-    echo -n -s $cwd ' ' $arrow ' ' $normal
+    echo -n $cwd $arrow $normal
 end
 
 
