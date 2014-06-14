e47d1dc9f60ff2882c35f153727748223ec404ee
sullenel <sullenerd@gmail.com>
2014-06-14 14:03:35 +0400

    Remove zsh-syntax-highlighting plugin

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,12 +6,3 @@
 if [[ -f /etc/arch-release ]]; then
     source /usr/share/doc/pkgfile/command-not-found.zsh
 fi
-
-
-if [[ -d $HOME/.zsh/plugins/zsh-syntax-highlighting/ ]]; then
-    source $HOME/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-    ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern root)
-    ZSH_HIGHLIGHT_STYLES[command]='fg=cyan,bold'
-    ZSH_HIGHLIGHT_STYLES[alias]='fg=cyan,bold'
-    ZSH_HIGHLIGHT_STYLES[builtin]='fg=cyan,bold'
-fi
