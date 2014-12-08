502c1ee0c55b6b2f43ce74cd69eca3ac7f9f8d45
sullenel <sullenerd@gmail.com>
2014-12-09 02:25:47 +0300

    Change prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,15 +1,9 @@
 function fish_prompt -d 'Prompt'
-    function _last_two_dirs
-      if [ $PWD != $HOME ];
-        echo -n $PWD | awk -F\/ '{print $(NF-1),$(NF)}' | sed 's/ /\\//'
-      end
-    end
-
     set -l red (set_color -o red)
-    set -l blue (set_color -o blue)
+    set -l green (set_color -o green)
     set -l normal (set_color normal)
-    set -l arrow "$red➜"
-    set -l cwd $blue(_last_two_dirs)
+    set -l arrow "$red›"
+    set -l cwd $green(basename (echo $PWD | sed -e "s|^$HOME||"))
 
     echo -n $cwd $arrow $normal
 end
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,6 +1,9 @@
 function fish_right_prompt -d 'Right prompt'
     set -l green (set_color green)
+    set -l yellow (set_color --bold yellow)
+    set -l normal (set_color normal)
     set -l files $green(ls -A | wc -l)
+    set -l branch $yellow(git rev-parse --abbrev-ref HEAD ^/dev/null)$normal
 
-    echo -n -s $files
+    echo -n $branch $files
 end
