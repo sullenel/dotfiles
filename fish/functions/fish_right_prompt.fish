d0d1c22dd9473873eacde25a6034245063485615
sullenel <sullenel@users.noreply.github.com>
2021-07-12 21:09:52 +0400

    Fix fish prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -3,7 +3,7 @@ function fish_right_prompt -d 'Right prompt'
     set -l yellow (set_color --bold yellow)
     set -l normal (set_color normal)
     set -l files $green(ls -A | wc -l)
-    set -l branch $yellow(git rev-parse --abbrev-ref HEAD ^/dev/null)$normal
+    set -l branch $yellow(git rev-parse --abbrev-ref HEAD 2>/dev/null)$normal
 
     echo -n $branch $files
 end
