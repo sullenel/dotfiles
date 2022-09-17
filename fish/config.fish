7b1ae8de7f25c990c54be738d4630279ea6eda7b
sullenel <sullenel@users.noreply.github.com>
2022-09-17 21:16:49 +0400

    Tweak fish config

--- fish/config.fish
+++ fish/config.fish
@@ -12,24 +12,22 @@ function fish_title;end
 # COLOURS
 # -----------------------------------------------------------------------------
 # set the colour of autocompletions
-set fish_color_autosuggestion yellow
+set fish_color_autosuggestion green
 
 # set the colour for commands
-set fish_color_command blue --bold
+set fish_color_command blue
 
 # set the colour for comments
-set fish_color_comment magenta
+set fish_color_comment grey
 
 # set the colour for search matches
 set fish_color_search_match normal
 
 
 # -----------------------------------------------------------------------------
-# VARIABLES
+# ENVIRONMENT
 # -----------------------------------------------------------------------------
-# Editor
 set -gx EDITOR nvim
 set -gx VISUAL $EDITOR
 
-# Terminal
 test $TMUX; and set -x TERM screen-256color; or set -x TERM xterm-256color
