e2c6eba20eb59ba6ae4625072828eabc60ff0d55
sullenel <sullenel@users.noreply.github.com>
2023-02-18 21:52:17 +0400

    Remove right prompt

--- zsh/prompts.zsh
+++ zsh/prompts.zsh
@@ -13,11 +13,11 @@ autoload -Uz add-zsh-hook
 add-zsh-hook precmd _precmd
 
 # Left prompt
-CWD='%F{147}%B%2~%b%f'
-BRANCH_NAME='%F{220}${vcs_info_msg_0_}%f'
-ARROW_SIGN='%F{197}%B›%b%f'
-prompt="$CWD$BRANCH_NAME $ARROW_SIGN "
+# Last 2 directories of CWD
+_CWD='%F{147}%B%2~%b%f'
 
-# Right prompt
-FILE_COUNT_IN_CWD='$(ls -A | wc -l)'
-RPROMPT="%F{green}$FILE_COUNT_IN_CWD%f"
+# Git branch name if CWD is a git repo
+_BRANCH_NAME='%F{220}${vcs_info_msg_0_}%f'
+_ARROW_SIGN='%F{197}%B›%b%f'
+
+prompt="$_CWD$_BRANCH_NAME $_ARROW_SIGN "
